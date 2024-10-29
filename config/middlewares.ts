700841e62fdd71611d0f9c5af003df553bf79500
efegorkemumit <47424323+efegorkemumit@users.noreply.github.com>
2024-10-29 16:05:05 +0300

    10- Bug Fix Strapi

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -2,7 +2,19 @@ export default [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: [
+        'http://localhost:3000', // React Native uygulamanın çalıştığı port
+        'http://10.0.2.2:3000', // Emülatör için
+        'http://192.168.8.106:3000', //  Bilgisayarın yerel IP adresi
+        'http://*', // Tüm kaynaklardan erişim için (geliştirme aşamasında kullanılabilir)*/
+      ],
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+      allowedHeaders: ['Content-Type', 'Authorization'],
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
